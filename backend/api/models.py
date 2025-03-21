f5a328a9082b2bc91d683595a372d11a4a877440
Dennis Peter <peterdennis573@gmail.com>
2025-03-21 20:20:53 +0300

    Pushed the project instantiations

---
+++ backend/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+from django.db import models
+
+
+class User(AbstractUser):
+
+    is_employer = models.BooleanField(default=False)
+
+    is_worker = models.BigIntegerField(default=False)
